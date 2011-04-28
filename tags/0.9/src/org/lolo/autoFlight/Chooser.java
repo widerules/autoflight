aad1e9194b9f96e94768b422b2b796bbb93c321c
l.gabardos@gmail.com <l.gabardos@gmail.com@d6d5012d-a8cd-2e7a-f828-93838f5aed96>
2011-04-28 07:53:08 +0000

    Version 0.9

---
+++ tags/0.9/src/org/lolo/autoFlight/Chooser.java
@@ -0,0 +1,71 @@
+package org.lolo.autoFlight;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Chooser extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.chooser);
+
+		// on récupère l'heure à laquelle on devait sonner
+		// SharedPreferences sp = getSharedPreferences("autoFlight",
+		// Activity.MODE_WORLD_WRITEABLE);
+		// String hour = sp.getString("hour", "");
+		// Long hourInMillis = Long.valueOf(hour);
+
+		// récupération du bouton oui
+		Button btnYes = (Button) findViewById(R.id.btnYes);
+		// récupération du bouton non
+		Button btnNo = (Button) findViewById(R.id.btnNo);
+		// récupération du chrono
+		final TextView chrono = (TextView) findViewById(R.id.textViewCountDown);
+		final CountDownTimer counter = new CountDownTimer(60000, 1000) {
+
+			@Override
+			public void onTick(long millisUntilFinished) {
+				chrono.setText("" + ((int) (millisUntilFinished / 1000)));
+			}
+
+			@Override
+			public void onFinish() {
+				launchFlightMode();
+			}
+		}.start();
+
+		btnYes.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				counter.cancel();
+				launchFlightMode();
+			}
+		});
+
+		btnNo.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				counter.cancel();
+				Chooser.this.finish();
+			}
+		});
+	}
+
+	private void launchFlightMode() {
+		Settings.System.putInt(Chooser.this.getApplicationContext()
+				.getContentResolver(), Settings.System.AIRPLANE_MODE_ON, 1);
+		Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+		intent.putExtra("state", 1);
+		Chooser.this.getApplicationContext().sendBroadcast(intent);
+
+		Chooser.this.finish();
+	}
+}
---
+++ tags/0.9/src/org/lolo/autoFlight/Main.java
@@ -0,0 +1,147 @@
+package org.lolo.autoFlight;
+
+import java.util.Calendar;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+/**
+ * Main permettant de setter l'heure à laquelle on va demander si le téléphone
+ * doit passer en mode avion.
+ * 
+ * @author lolo
+ * 
+ */
+public class Main extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		final TimePicker tp = (TimePicker) findViewById(R.id.tpFlightTime);
+		// on récupère l'heure actuelle stockée (ou pas)
+		final SharedPreferences sp = getSharedPreferences("autoFlight",
+				Activity.MODE_WORLD_WRITEABLE);
+		
+		initTimePicker();
+		
+        // 
+        Button btnSave = (Button) findViewById(R.id.btnSavePref);
+        btnSave.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Integer hour = tp.getCurrentHour();
+				Integer min = tp.getCurrentMinute();
+				Calendar cal = Calendar.getInstance();
+				
+				if (hour < cal.get(Calendar.HOUR_OF_DAY) || (hour == cal.get(Calendar.HOUR_OF_DAY) && min < cal.get(Calendar.MINUTE))) {
+					cal.roll(Calendar.DAY_OF_MONTH, 1);
+				}
+				cal.set(Calendar.HOUR_OF_DAY, hour.intValue());
+				cal.set(Calendar.MINUTE, min.intValue());
+				cal.set(Calendar.SECOND, 0);
+				
+				sp.edit().putString("hour", "" + cal.getTimeInMillis()).commit();
+				
+				Toast.makeText(Main.this, "Heure réglée sur " + hour + ":" + min, Toast.LENGTH_LONG).show();
+				
+		        initAskerInTime();
+			}
+		});
+        
+        initAskerInTime();
+    }
+
+	/**
+	 * Initialisation du timepicker avec l'heure qui a déjà été saisie... ou pas
+	 */
+	private void initTimePicker() {
+		// on récupère l'heure actuelle stockée (ou pas)
+		SharedPreferences sp = getSharedPreferences("autoFlight",
+				Activity.MODE_WORLD_WRITEABLE);
+		// et voici l'heure... En timmeInMillis of course
+		String hour = sp.getString("hour", "");
+
+		Calendar cal = Calendar.getInstance();
+		
+		if ("".equals(hour)) {
+			cal.roll(Calendar.DAY_OF_MONTH, 1);
+			cal.set(Calendar.HOUR_OF_DAY, 0);
+			cal.set(Calendar.MINUTE, 0);
+			
+		} else {
+			Long hourInMillis = Long.valueOf(hour);
+			Calendar savedTime = Calendar.getInstance();
+			savedTime.setTimeInMillis(hourInMillis);
+			
+			cal.set(Calendar.HOUR_OF_DAY, savedTime.get(Calendar.HOUR_OF_DAY));
+			cal.set(Calendar.MINUTE, savedTime.get(Calendar.MINUTE));
+			
+			if (cal.getTimeInMillis() < Calendar.getInstance().getTimeInMillis()) {
+				cal.roll(Calendar.DAY_OF_MONTH, 1);
+			}
+		}
+
+		final TimePicker tp = (TimePicker) findViewById(R.id.tpFlightTime);
+		tp.setCurrentHour(cal.get(Calendar.HOUR_OF_DAY));
+		tp.setCurrentMinute(cal.get(Calendar.MINUTE));
+	}
+
+	/**
+	 * Initialisation de l'alarme du mode avion
+	 */
+	private void initAskerInTime() {
+		// on récupère l'heure actuelle stockée (ou pas)
+		SharedPreferences sp = getSharedPreferences("autoFlight",
+				Activity.MODE_WORLD_WRITEABLE);
+		// et voici l'heure... En timmeInMillis of course
+		String hour = sp.getString("hour", "");
+
+		// a-t-on déjà configuré l'heure du mode avion
+		if ("".equals(hour)) {
+			Calendar tomorrow = Calendar.getInstance();
+			tomorrow.roll(Calendar.DAY_OF_MONTH, 1);
+			tomorrow.set(Calendar.HOUR_OF_DAY, 0);
+			tomorrow.set(Calendar.MINUTE, 0);
+			
+			hour = "" + tomorrow.getTimeInMillis();
+		}
+
+		Long hourInMillis = Long.valueOf(hour);
+
+		// Récupération de l'instance du service AlarmManager.
+		AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+		// On instancie l'Intent qui va être appelé au moment du reveil.
+		Intent intent = new Intent(this, AlarmReceiver.class);
+
+		// On créer le pending Intent qui identifie l'Intent de reveil avec un
+		// ID et un/des flag(s)
+		PendingIntent pendingintent = PendingIntent.getBroadcast(this, 0,
+				intent, 0);
+
+		// On annule l'alarm pour replanifier si besoin
+		am.cancel(pendingintent);
+
+		// ok maintenant, on va faire la différence entre l'heure actuelle et
+		// celle choisie
+		// long when = hourInMillis - cal.getTimeInMillis();
+
+		// On ajoute le reveil au service de l'AlarmManager
+		am.setRepeating(AlarmManager.RTC_WAKEUP, hourInMillis,
+				AlarmManager.INTERVAL_DAY, pendingintent);
+	}
+}
